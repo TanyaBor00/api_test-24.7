14ac6a1a98e4d40fb00619874b9f27b94ee2ffa2
TanyaBor00 <162904885+TanyaBor00@users.noreply.github.com>
2024-07-05 08:51:41 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,19 @@
+valid_email = "fyghnrth@mail.ru"
+valid_password = "1234"
+valid_email_1 = "fghbdfghngnh@mail.ru"
+valid_password_1 = "123"
+email_invalid = "ghjgkdl@mail.ru"
+password_invalid = '1098'
+
+
+name_space = ' '
+name_very_long = 'AAAAAAAAAAaaaaaaaaaaannnnnnnnnnnnsssssssssssseeeeeeeeeeeeepppppppppppppppppppppppp;;;;;;;;;;;;;'
+name = "Poti"
+animal_type = "Dog"
+age = "5"
+pet_photo = "images/pet_photo.jpeg"
+dog_photo = "images/dog.png"
+dog_photo_gif = "images/dog_photo_gif.gif"
+pet_txt = "images/pet.txt"
+
+pet_id = '72366c00-e20f-436c-9b2c-854bc9442718'
